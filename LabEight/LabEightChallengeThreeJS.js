4dba5f8c8f4c4c4f7070c63593114d5be8e433d4
lauramaieron <54905442+lauramaieron@users.noreply.github.com>
2019-11-08 18:42:53 -0500

    Add files via upload

---
+++ LabEight/LabEightChallengeThreeJS.js
@@ -0,0 +1,9 @@
+//This will output when the page loads
+document.write("Zero");
+
+//This will output when a button is pressed
+function theRest(){
+	document.getElementById("output1").innerHTML="One";
+	document.getElementById("output2").innerHTML="Two";
+	document.getElementById("output3").innerHTML="Three";
+}
